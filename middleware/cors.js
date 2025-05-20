1b6ad0c6b06d0693c6fddc5518eb9ab9977dd0ef
usernamedinesh <borod9200@gmail.com>
2025-05-20 18:27:09 +0530

    modify cors port for client

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 function corsMiddleware(req, res, next) {
   cors({
-    origin: "http://localhost:3000", // restrict calls to this address
+    origin: "http://localhost:5173", // restrict calls to this address
     methods: "GET", // only allow GET requests
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept", // allowed headers
   })(req, res, next); // Call cors middleware function
