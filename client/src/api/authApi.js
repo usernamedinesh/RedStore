6e91f6978807732aa4c3354c343c0d4b0026ee22
usernamedinesh <borod9200@gmail.com>
2025-05-20 18:27:44 +0530

    added api file for fetching apis

---
+++ client/src/api/authApi.js
@@ -0,0 +1,57 @@
+// here all api calls for auth
+import axios from "axios";
+import { API_URL } from "./api.js";
+
+/*
+ * VERIFY EMAIL OR PHONE
+ * This api will run first
+ * It will take either email or phone
+ * and send the otp to the email and phone(which is provied)
+ * and return the response
+ */
+export const verifyEmailOrPhone = async (emailOrPhone) => {
+  try {
+    const response = await axios.post(`${API_URL}/api/verify`, {
+      phoneOremail: emailOrPhone,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error verifying email or phone:", error);
+    throw error;
+  }
+};
+
+/*
+ * REGISTER
+ * After the successfully return  response of verification email or phone
+ * This route will run
+ * It will take the otp and other details
+ */
+export const register = async (userdata) => {
+  try {
+    const response = await axios.post(`${API_URL}/api/register`, {
+      ...userdata,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
+
+/*
+ * LOGIN
+ * take the email and password
+ */
+
+export const login = async (userdata) => {
+  try {
+    const response = await axios.post(`${API_URL}/api/login`, {
+      ...userdata,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error logging user:", error);
+    throw error;
+  }
+};
