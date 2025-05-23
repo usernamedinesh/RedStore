15d2219bda39976977ef0be1b8276a97f77c7be7
usernamedinesh <borod9200@gmail.com>
2025-05-23 11:55:19 +0530

    added dark and white mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
+import router from "./routes.jsx";
 import { StrictMode } from "react";
+import { RouterProvider } from "react-router";
+import { ThemeProvider } from "./context/ThemeProvider";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </ThemeProvider>
   </StrictMode>,
 );
