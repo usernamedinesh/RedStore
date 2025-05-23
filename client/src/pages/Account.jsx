a0c49a401f7ff6bc3163ab47508312f7986c3fdc
usernamedinesh <borod9200@gmail.com>
2025-05-23 22:55:55 +0530

    add logout page

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,17 +1,26 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/slice/auth/authSlice";
 
 function Account() {
   document.title = "profile";
   const navigate = useNavigate();
   const location = useLocation();
+  const dispatch = useDispatch();
   const { userId, token } = useSelector((state) => state.auth);
+
   useEffect(() => {
     if ((!userId || !token) && location.pathname !== "/login") {
       navigate("/login", { replace: true }); // `replace: true` prevents going "back" to protected page
     }
   }, [userId, token, navigate]);
+
+  // logout func
+  const Handlelogout = () => {
+    dispatch(logout());
+  };
 
   return (
     <>
@@ -21,6 +30,11 @@ function Account() {
           <p> UserId: {userId} </p>
           <p> Token: {token} </p>
         </div>
+        {!userId && (
+          <div>
+            <button onClick={Handlelogout}>LOGOUT</button>
+          </div>
+        )}
       </div>
     </>
   );
