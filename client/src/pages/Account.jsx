05836fc6d564196bb4b045fffa5fe842a27714e4
usernamedinesh <borod9200@gmail.com>
2025-05-26 00:36:16 +0530

    add logout working

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
+import { useDispatch } from "../redux/store";
 import { logout } from "../redux/slice/auth/authSlice";
 
 function Account() {
@@ -24,15 +25,20 @@ function Account() {
 
   return (
     <>
-      <div>
+      <div className="bg-[var(--my-bg)] text-black dark:bg-[var(--my-bg)]  dark:text-white mb-3.5 ">
         <div>Account Details</div>
         <div>
           <p> UserId: {userId} </p>
           <p> Token: {token} </p>
         </div>
-        {!userId && (
+        {userId && (
           <div>
-            <button onClick={Handlelogout}>LOGOUT</button>
+            <button
+              onClick={Handlelogout}
+              className=" font-bold   fixed bottom-6 right-6 z-50 dark:bg-gray-800 bg-gray-900  text-red-500 px-4 py-2 rounded-full shadow-lg hover:bg-gray-400 transition dark:hover:bg-gray-900"
+            >
+              LOGOUT
+            </button>
           </div>
         )}
       </div>
