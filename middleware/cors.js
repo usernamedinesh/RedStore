f72cb60d7d83696eb8a86385c5f875bd33bcc9f4
usernamedinesh <borod9200@gmail.com>
2025-05-25 23:48:59 +0530

    update corss

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,9 +2,11 @@ const cors = require("cors");
 
 function corsMiddleware(req, res, next) {
   cors({
-    origin: "http://localhost:5173", // restrict calls to this address
-    methods: "GET", // only allow GET requests
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept", // allowed headers
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    // allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })(req, res, next); // Call cors middleware function
 }
 
