21ec3825299a63c15b027da8a162b7310006f56a
usernamedinesh <borod9200@gmail.com>
2025-05-26 00:57:06 +0530

    add owner register request  button

--- client/src/customComponents/FloatingSingupBtn.jsx
+++ client/src/customComponents/FloatingSingupBtn.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 function FloatingSignupButton() {
   return (
-    <div className=" font-bold   fixed bottom-6 right-6 z-50 dark:bg-gray-400 bg-gray-900  text-red-500 px-4 py-2 rounded-full shadow-lg hover:bg-gray-400 transition dark:hover:bg-gray-900">
+    <div className=" font-bold   fixed bottom-18 right-6 z-50 dark:bg-gray-400 bg-gray-900  text-white hover:text-black  px-4 py-2 rounded-full shadow-lg hover:bg-gray-400 transition dark:hover:bg-gray-900 dark:hover:text-white dark:text-black">
       <NavLink to="/signup" end>
         SIGNUP
       </NavLink>
