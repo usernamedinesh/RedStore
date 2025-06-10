c77c0e60f8f0f2fff28a0d410daa7dc603c86c43
usernamedinesh <borod9200@gmail.com>
2025-06-10 19:32:58 +0530

    port changes & socket connected

--- owner-dashbaord/src/components/Chat.jsx
+++ owner-dashbaord/src/components/Chat.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "preact/hooks";
-
 import { useSocket } from "../customComponents/socketContext.jsx";
+import axios from "axios";
+
 export function Chat() {
   const socket = useSocket();
   useEffect(() => {
@@ -10,11 +11,33 @@ export function Chat() {
       console.log("Connected to server");
     });
 
+    //or do i need to fetch here? user
+    const fetchUsers = async (userId) => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/chat/partners",
+          { userId },
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+
     return () => {
       socket.disconnect();
       console.log("Disconnected from server");
     };
   });
+
+  // i need to fetch all users annd their messages
+  //TODO:
+
   return (
     <>
       <div></div>
