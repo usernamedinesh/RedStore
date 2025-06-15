6c00e007c916e871cc1c124774fb3ea2fae1f149
usernamedinesh <borod9200@gmail.com>
2025-06-15 08:04:06 +0530

    updated from s3 to new v3 version

--- config/awsConfig.js
+++ config/awsConfig.js
@@ -1,11 +1,17 @@
-const AWS = require("aws-sdk");
+// const AWS = require("aws-sdk");
+const { S3Client } = require("@aws-sdk/client-s3");
 const env = require("./envConfig");
 
-AWS.config.update({
+// AWS.config.update({
+//   region: env.AWS_REGION,
+//   accessKeyId: env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+// });
+//
+// const s3 = new AWS.S3();
+const s3 = new S3Client({
   region: env.AWS_REGION,
   accessKeyId: env.AWS_ACCESS_KEY_ID,
   secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
 });
-
-const s3 = new AWS.S3();
 module.exports = s3;
