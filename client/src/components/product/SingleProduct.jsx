497d779c1fb5c0f8957f7633ee61b06da647b39c
usernamedinesh <borod9200@gmail.com>
2025-06-17 18:47:18 +0530

    changes in color buy now

--- client/src/components/product/SingleProduct.jsx
+++ client/src/components/product/SingleProduct.jsx
@@ -221,7 +221,7 @@ export const SingleProduct = () => {
             // className="mr-10 px-4 py-2 dark:bg-orange-600 rounded-md bg-orange-600 text-white
             //        transform transition-transform duration-300 ease-in-out
             //        hover:scale-105 hover:shadow-lg"
-            className="mr-10 text-gray-900 dark:text-white 
+            className="mr-10 text-gray-900 dark:text-black 
              bg-gradient-to-r from-teal-300 to-lime-300 
              hover:bg-gradient-to-l hover:from-teal-400 hover:to-lime-400 
              focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-teal-700 
@@ -233,7 +233,7 @@ export const SingleProduct = () => {
             add to cart
           </button>
           <button
-            className="mr-10 text-gray-900 dark:text-white 
+            className="mr-10 text-gray-900 dark:text-black 
              bg-gradient-to-r from-teal-300 to-lime-300 
              hover:bg-gradient-to-l hover:from-teal-400 hover:to-lime-400 
              focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-teal-700 
